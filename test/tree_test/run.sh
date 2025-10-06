208595399385f8579e6095bb1a3e42f888057ed0
sofiperez91 <sperezcasulo@gmail.com>
2025-10-05 22:05:48 -0300

    add run.sh for link prediction

--- test/tree_test/run.sh
+++ test/tree_test/run.sh
@@ -2,3 +2,4 @@ python -m test.tree_test.main --embedding_type='poincare_maps' --output_space='p
 python -m test.tree_test.main --embedding_type='poincare_maps' --output_space='spherical'
 
 python -m test.tree_test.link_prediction --embedding_type="poincare_maps" --q=0.9 --n_links=20
+python -m test.tree_test.link_prediction --embedding_type="poincare_embeddings" --q=0.9 --n_links=20
